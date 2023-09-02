fff026c21a1fb2b2d71623fd700cbf376ec5862d
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 06:22:34 +0900

    BingGame エンティティ作成

---
+++ bingo-web/domains/BingoGame/models/BingoGame.ts
@@ -0,0 +1,34 @@
+const BingoGameStateEnum = {
+  CREATED: 0,
+  PLAYING: 1,
+  FINISHED: 2,
+} as const;
+
+type BingoGameStateName = keyof typeof BingoGameStateEnum;
+type BingoGameState = (typeof BingoGameStateEnum)[BingoGameStateName];
+
+type BingoGameDto = {
+  id: string;
+  lotteryNumbers: number[];
+  viewId: string;
+  state: BingoGameState;
+  hashedManagementPassword: string | null;
+};
+
+class BingoGame {
+  private constructor(private dto: BingoGameDto) {}
+
+  static createGame() {
+    return new BingoGame({
+      id: crypto.randomUUID(),
+      lotteryNumbers: [],
+      viewId: crypto.randomUUID(),
+      hashedManagementPassword: null,
+      state: BingoGameStateEnum.CREATED,
+    });
+  }
+
+  static fromDto(dto: BingoGameDto) {
+    return new BingoGame({ ...dto });
+  }
+}
