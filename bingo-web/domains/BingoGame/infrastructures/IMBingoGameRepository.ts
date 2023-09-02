9ec34b645851a9e0992b6dac405b58b789c6b11e
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 07:19:45 +0900

    BingoGameRepository のインメモリ実装作成

--- bingo-web/domains/BingoGame/infrastructures/IMBingoGameRepository.ts
+++ bingo-web/domains/BingoGame/infrastructures/IMBingoGameRepository.ts
@@ -1,6 +1,8 @@
+import { BingoGame, BingoGameDto } from "../models/BingoGame";
+
 const store = new Map<string, BingoGameDto>();
 
-class InMemoryGameRepository implements BingoGameRepository {
+export class InMemoryGameRepository implements BingoGameRepository {
   async save(bingoGame: BingoGame): Promise<void> {
     store.set(bingoGame.id, bingoGame.toDto());
   }
