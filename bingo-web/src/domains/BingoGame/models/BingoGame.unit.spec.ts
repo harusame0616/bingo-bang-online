83e92622c5586cfe44736ef8b777cd7e63458969
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 09:02:46 +0900

    drawLotteryNumber のユニットテストを作成

--- bingo-web/src/domains/BingoGame/models/BingoGame.unit.spec.ts
+++ bingo-web/src/domains/BingoGame/models/BingoGame.unit.spec.ts
@@ -16,4 +16,51 @@ describe("BingoGame", () => {
       });
     });
   });
+
+  describe("drawLotteryNumber", () => {
+    it("抽選番号が重複しない", () => {
+      const bingoGame = BingoGame.createGame();
+
+      [...new Array(75)].forEach(() => {
+        bingoGame.drawLotteryNumber();
+      });
+
+      const set = new Set(bingoGame.lotteryNumbers);
+      expect(set.size).toBe(75);
+    });
+
+    it("抽選番号が 1-75 の間", () => {
+      const bingoGame = BingoGame.createGame();
+
+      [...new Array(75)].forEach(() => {
+        bingoGame.drawLotteryNumber();
+      });
+
+      expect(
+        bingoGame.lotteryNumbers.every(
+          (lotteryNumber) => lotteryNumber > 0 && lotteryNumber <= 75
+        )
+      ).toBe(true);
+    });
+
+    it("75回抽選ができる", () => {
+      const bingoGame = BingoGame.createGame();
+
+      [...new Array(75)].forEach(() => {
+        bingoGame.drawLotteryNumber();
+      });
+
+      expect(bingoGame.lotteryNumbers.length).toBe(75);
+    });
+
+    it("76回抽選するとエラーが出る", () => {
+      const bingoGame = BingoGame.createGame();
+
+      [...new Array(75)].forEach(() => {
+        bingoGame.drawLotteryNumber();
+      });
+
+      expect(() => bingoGame.drawLotteryNumber()).toThrow();
+    });
+  });
 });
