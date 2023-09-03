284ae0d5d318b8cd1a78cd05c3c53b6e3aa4c89c
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 14:28:47 +0900

    抽選を実装

--- bingo-web/src/app/games/[bingoGameId]/page.tsx
+++ bingo-web/src/app/games/[bingoGameId]/page.tsx
@@ -1,9 +1,40 @@
+import { BingoGameDrawLotteryNumberUsecase } from "@/domains/BingoGame/usecases/BingoGameDrawLotteryNumber.usecase";
+import { getRepository } from "@/lib/getRepository";
+
 type Props = {
   params: {
     bingoGameId: string;
   };
 };
 
+async function drawLotteryNumber(formData: FormData) {
+  "use server";
+
+  const bingoGameId = formData.get("bingoGameId");
+
+  if (!bingoGameId) {
+    throw new Error("bingoGameId is required");
+  }
+
+  if (typeof bingoGameId !== "string") {
+    throw new Error("bingoGameId is invalid type");
+  }
+
+  const drawLotteryNumberUsecase = new BingoGameDrawLotteryNumberUsecase(
+    getRepository("bingoGame")
+  );
+  await drawLotteryNumberUsecase.execute(bingoGameId);
+}
+
 export default async function GameNewPage({ params: { bingoGameId } }: Props) {
-  return <div>{bingoGameId} </div>;
+  return (
+    <div>
+      <h1>Bingo Game</h1>
+      <div>{bingoGameId}</div>
+      <form action={drawLotteryNumber}>
+        <input type="text" name="bingoGameId" hidden value={bingoGameId} />
+        <button>番号を抽選する</button>
+      </form>
+    </div>
+  );
 }
