9906a96bcf544ba919dc6c761ed7663ae0a2eba1
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 09:15:52 +0900

    インメモリの BingoGameRepository.findOneById を実装

--- bingo-web/src/domains/BingoGame/infrastructures/IMBingoGameRepository.ts
+++ bingo-web/src/domains/BingoGame/infrastructures/IMBingoGameRepository.ts
@@ -1,8 +1,18 @@
 import { BingoGame, BingoGameDto } from "../models/BingoGame";
+import { BingoGameRepository } from "../usecases/BingoGameRepository";
 
 const store = new Map<string, BingoGameDto>();
 
 export class InMemoryGameRepository implements BingoGameRepository {
+  async findOneById(bingoGameId: string): Promise<BingoGame> {
+    const bingoGame = store.get(bingoGameId);
+    if (!bingoGame) {
+      throw new Error("ビンゴゲームが見つかりません");
+    }
+
+    return BingoGame.fromDto(bingoGame);
+  }
+
   async save(bingoGame: BingoGame): Promise<void> {
     store.set(bingoGame.id, bingoGame.toDto());
   }
