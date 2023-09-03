36582f204290fb8ff36486b51eba3ddc4d789d7f
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 09:14:54 +0900

    BingoGame の抽選ユースケース作成

--- bingo-web/src/domains/BingoGame/usecases/BingoGameRepository.ts
+++ bingo-web/src/domains/BingoGame/usecases/BingoGameRepository.ts
@@ -1,3 +1,6 @@
-type BingoGameRepository = {
+import { BingoGame } from "../models/BingoGame";
+
+export type BingoGameRepository = {
+  findOneById: (bingoGameId: string) => Promise<BingoGame>;
   save: (bingoGame: BingoGame) => Promise<void>;
 };
