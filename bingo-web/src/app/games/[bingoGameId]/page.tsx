dfe5369162a4539912905063da03fc83b289aa6e
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-03 16:47:07 +0900

    ビンゴゲーム情報表示

--- bingo-web/src/app/games/[bingoGameId]/page.tsx
+++ bingo-web/src/app/games/[bingoGameId]/page.tsx
@@ -1,5 +1,9 @@
+import { BingoGameStateEnum } from "@/domains/BingoGame/models/BingoGame";
 import { BingoGameDrawLotteryNumberUsecase } from "@/domains/BingoGame/usecases/BingoGameDrawLotteryNumber.usecase";
+import { BingoGameFindOneUsecase } from "@/domains/BingoGame/usecases/BingoGameFindOne.usecase";
 import { getRepository } from "@/lib/getRepository";
+import { revalidatePath } from "next/cache";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: {
@@ -24,9 +28,19 @@ async function drawLotteryNumber(formData: FormData) {
     getRepository("bingoGame")
   );
   await drawLotteryNumberUsecase.execute(bingoGameId);
+  revalidatePath("/game/[bingoGameId]");
 }
 
 export default async function GameNewPage({ params: { bingoGameId } }: Props) {
+  const bingoGameFindOneUsecase = new BingoGameFindOneUsecase(
+    getRepository("bingoGame")
+  );
+  const bingoGame = await bingoGameFindOneUsecase.execute(bingoGameId);
+
+  if (!bingoGame) {
+    return notFound();
+  }
+
   return (
     <div>
       <h1>Bingo Game</h1>
@@ -38,8 +52,20 @@ export default async function GameNewPage({ params: { bingoGameId } }: Props) {
           hidden
           defaultValue={bingoGameId}
         />
-        <button>番号を抽選する</button>
+        <button disabled={bingoGame.state === BingoGameStateEnum.FINISHED}>
+          番号を抽選する
+        </button>
       </form>
+      {bingoGame.state === BingoGameStateEnum.FINISHED ? (
+        <div>抽選終了</div>
+      ) : (
+        <div>抽選中</div>
+      )}
+      <div className="flex flex-wrap gap-8">
+        {bingoGame.lotteryNumbers.map((lotteryNumber) => (
+          <div key={lotteryNumber}>{lotteryNumber}</div>
+        ))}
+      </div>
     </div>
   );
 }
