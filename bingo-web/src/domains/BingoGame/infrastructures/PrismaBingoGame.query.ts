eb645cdf97bac4620d8714dc36a0a3d7a07af46f
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-13 06:20:05 +0900

    Prisma のインフラ作成

---
+++ bingo-web/src/domains/BingoGame/infrastructures/PrismaBingoGame.query.ts
@@ -0,0 +1,44 @@
+import { prisma } from '@/lib/prisma';
+
+import {
+  BingoGameDtoWithCards,
+  BingoGameQuery,
+} from '../usecases/BingoGame.query';
+
+export class PrismaBingoGameQuery implements BingoGameQuery {
+  async findOneByIdWithCards(
+    bingoGameId: string,
+  ): Promise<BingoGameDtoWithCards | null> {
+    const [bingoGame, bingoCards] = await Promise.all([
+      prisma.bingoGameEntity.findUnique({
+        where: { id: bingoGameId },
+      }),
+      prisma.bingoCardEntity.findMany({
+        where: { bingoGameId: bingoGameId },
+      }),
+    ]);
+
+    console.log({ bingoGame, bingoCards: JSON.stringify(bingoCards) });
+    if (!bingoGame) {
+      return null;
+    }
+
+    return {
+      ...bingoGame,
+      state: 'created',
+      bingoCards:
+        bingoCards?.map((card) => ({
+          id: card.id,
+          squares: [
+            card.squares.slice(0, 5),
+            card.squares.slice(5, 10),
+            card.squares.slice(10, 15),
+            card.squares.slice(15, 20),
+            card.squares.slice(20, 25),
+          ],
+          name: card.name,
+          bingoGameId: card.bingoGameId,
+        })) || [],
+    };
+  }
+}
---
+++ bingo-web/src/domains/BingoGame/infrastructures/PrismaBingoGame.repository.ts
@@ -0,0 +1,33 @@
+import { prisma } from '@/lib/prisma';
+
+import { BingoGame } from '../models/BingoGame';
+import { BingoGameRepository } from '../usecases/BingoGame.repository';
+
+export class PrismaBingoGameRepository implements BingoGameRepository {
+  async findOneById(bingoGameId: string): Promise<BingoGame | null> {
+    const bingoGame = await prisma.bingoGameEntity.findUnique({
+      where: { id: bingoGameId },
+    });
+    if (!bingoGame) {
+      return null;
+    }
+
+    return BingoGame.fromDto({
+      id: bingoGame.id,
+      lotteryNumbers: bingoGame.lotteryNumbers,
+      viewId: bingoGame.viewId,
+      hashedManagementPassword: null,
+      state: 'created',
+    });
+  }
+
+  async save(bingoGame: BingoGame): Promise<void> {
+    const { hashedManagementPassword, state, ...bingoGameDto } =
+      bingoGame.toDto();
+    await prisma.bingoGameEntity.upsert({
+      where: { id: bingoGame.id },
+      create: bingoGameDto,
+      update: bingoGameDto,
+    });
+  }
+}
