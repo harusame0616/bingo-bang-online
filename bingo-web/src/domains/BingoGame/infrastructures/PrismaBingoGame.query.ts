5775235d18c1849914b0122d325773db7f4318d5
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-13 19:16:26 +0900

    viewId の検索クエリ作成

--- bingo-web/src/domains/BingoGame/infrastructures/PrismaBingoGame.query.ts
+++ bingo-web/src/domains/BingoGame/infrastructures/PrismaBingoGame.query.ts
@@ -3,9 +3,46 @@ import { prisma } from '@/lib/prisma';
 import {
   BingoGameDtoWithCards,
   BingoGameQuery,
+  BingoGameViewDtoWithCards,
 } from '../usecases/BingoGame.query';
 
 export class PrismaBingoGameQuery implements BingoGameQuery {
+  async findOneByViewIdWithCards(
+    viewId: string,
+  ): Promise<BingoGameViewDtoWithCards | null> {
+    const bingoGame = await prisma.bingoGameEntity.findUnique({
+      where: { viewId },
+    });
+
+    if (!bingoGame) {
+      return null;
+    }
+
+    const bingoCards = await prisma.bingoCardEntity.findMany({
+      where: { bingoGameId: bingoGame.id },
+    });
+
+    const { id, ...bingoGameWithoutId } = bingoGame;
+
+    return {
+      ...bingoGameWithoutId,
+      state: 'created',
+      bingoCards:
+        bingoCards?.map((card) => ({
+          id: card.id,
+          squares: [
+            card.squares.slice(0, 5),
+            card.squares.slice(5, 10),
+            card.squares.slice(10, 15),
+            card.squares.slice(15, 20),
+            card.squares.slice(20, 25),
+          ],
+          name: card.name,
+          bingoGameId: card.bingoGameId,
+        })) || [],
+    };
+  }
+
   async findOneByIdWithCards(
     bingoGameId: string,
   ): Promise<BingoGameDtoWithCards | null> {
