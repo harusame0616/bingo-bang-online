0ca374452f483e0fa1b384fb06cce16901991ef4
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-13 09:00:45 +0900

    ビンゴゲーム開始ボタン

--- bingo-web/src/app/_components/BingoGameStartButton.tsx
+++ bingo-web/src/app/_components/BingoGameStartButton.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import { experimental_useFormStatus as useFormStatus } from 'react-dom';
-
 import { Button } from '@/components/Button';
 
 export default function BingoGameStartForm() {
-  const { pending } = useFormStatus();
-
   return (
-    <Button disabled={pending}>
-      {pending ? 'ビンゴゲームを準備中です' : '新しいビンゴゲームを開始する'}
+    <Button
+      disableInAction={true}
+      disableInActionChildren="ビンゴゲームを準備中です"
+    >
+      新しいビンゴゲームを開始する
     </Button>
   );
 }
