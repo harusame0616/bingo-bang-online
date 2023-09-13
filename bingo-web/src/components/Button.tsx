b3f43481b8ef54d59c2cd1c2a21650f9b7c400cc
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-13 09:00:45 +0900

    Button をサーバーアクション処理中は disabled にできるように変更

--- bingo-web/src/components/Button.tsx
+++ bingo-web/src/components/Button.tsx
@@ -1,30 +1,48 @@
 'use client';
 
+import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+
 type ButtonProps = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
->;
+> & {
+  disableInAction?: boolean;
+  disableInActionChildren?: React.ReactNode;
+};
 
-export function Button(props: ButtonProps) {
+function BaseButton(props: ButtonProps) {
   const { children, ...rest } = props;
+  const { pending } = useFormStatus();
+
+  const showChildren =
+    props.disableInAction === true && pending && props.disableInActionChildren
+      ? props.disableInActionChildren
+      : children;
+
   return (
     <button
       {...rest}
-      className={`bg-primary-normal hover:bg-primary-darker text-white font-bold py-2 px-4 rounded disabled:opacity-40 disabled:hover:bg-primary-normal`}
+      disabled={props.disabled || (props.disableInAction && pending)}
     >
-      {children}
+      {showChildren}
     </button>
   );
 }
 
-export function ButtonOutline(props: ButtonProps) {
-  const { children, ...rest } = props;
+export function Button(props: ButtonProps) {
   return (
-    <button
-      {...rest}
+    <BaseButton
+      {...props}
+      className={`bg-primary-normal hover:bg-primary-darker text-white font-bold py-2 px-4 rounded disabled:opacity-40 disabled:hover:bg-primary-normal`}
+    />
+  );
+}
+
+export function ButtonOutline(props: ButtonProps) {
+  return (
+    <BaseButton
+      {...props}
       className="border-primary-normal hover:bg-primary-darker hover:text-white border  text-primary-darker font-bold py-2 px-4 rounded disabled:opacity-20"
-    >
-      {children}
-    </button>
+    />
   );
 }
