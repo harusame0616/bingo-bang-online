12b28ce0a075ed4946cfdcea41199d4787997560
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-13 19:10:27 +0900

    viewId の検索クエリ作成

--- bingo-web/src/domains/BingoGame/usecases/BingoGame.query.ts
+++ bingo-web/src/domains/BingoGame/usecases/BingoGame.query.ts
@@ -2,6 +2,13 @@ import { BingoCardDto } from '@/domains/BingoCard/models/BingoCard';
 
 import { BingoGameDto } from '../models/BingoGame';
 
+export type BingoGameViewDtoWithCards = Omit<
+  BingoGameDto,
+  'hashedManagementPassword' | 'bingoCardIds' | 'id'
+> & {
+  bingoCards: Array<BingoCardDto>;
+};
+
 export type BingoGameDtoWithCards = Omit<
   BingoGameDto,
   'hashedManagementPassword' | 'bingoCardIds'
@@ -10,6 +17,10 @@ export type BingoGameDtoWithCards = Omit<
 };
 
 export type BingoGameQuery = {
+  findOneByViewIdWithCards: (
+    bingoGameId: string,
+  ) => Promise<BingoGameViewDtoWithCards | null>;
+
   findOneByIdWithCards: (
     bingoGameId: string,
   ) => Promise<BingoGameDtoWithCards | null>;
