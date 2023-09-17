3c59bc72a27c20a820c5ca21dc37279ecb0bedfc
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2023-09-17 14:41:47 +0900

    number が変更されたタイミングでルーレットを停止するように変更
    今まで
    - ボタンが押されてルーレット停止
    - サーバーアクション実行
    - 抽選結果反映
    
    今回の変更
    - ボタンが押される
    - サーバーアクション実行
    - 抽選結果反映 & ルーレット停止

--- bingo-web/src/app/games/[bingoGameId]/_components/LotteryRoulette.tsx
+++ bingo-web/src/app/games/[bingoGameId]/_components/LotteryRoulette.tsx
@@ -18,7 +18,7 @@ export default function LotteryRoulette({ finish, number }: Props) {
   const [timer, setTimer] = useState<number | undefined>(undefined);
   const [isRouletteStart, setIsRouletteStart] = useState(false);
   const [spiningAudio] = useState(new Audio('/se/spinning.mp3'));
-  // const [stopAudio] = useState(new Audio('/se/stop.mp3'));
+  const [stopAudio] = useState(new Audio('/se/stop.mp3'));
 
   useEffect(() => {
     spiningAudio.loop = true;
@@ -27,34 +27,27 @@ export default function LotteryRoulette({ finish, number }: Props) {
 
   useEffect(() => {
     if (!isRouletteStart) {
-      clearInterval(timer);
       return;
     }
+
+    setIsRouletteStart(false);
+    clearInterval(timer);
+
+    spiningAudio.pause();
+    stopAudio.play();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [number]);
+
+  const startRoulette = async () => {
+    await spiningAudio.play();
+    setIsRouletteStart(true);
 
     const newTimer = window.setInterval(() => {
       setLotteryNumber(generateLotteryNumber());
     }, 100);
 
     setTimer(newTimer);
-
-    return () => clearInterval(timer);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isRouletteStart]);
-
-  useEffect(() => {
-    setIsRouletteStart(false);
-  }, [number]);
-
-  const startRoulette = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    // await spiningAudio.play();
-    setIsRouletteStart(true);
-  };
-
-  const stopRoulette = async () => {
-    // spiningAudio.pause();
-    // await stopAudio.play();
-    setIsRouletteStart(false);
   };
 
   return (
@@ -77,15 +70,11 @@ export default function LotteryRoulette({ finish, number }: Props) {
 
       <div className=" flex justify-center">
         {isRouletteStart ? (
-          <Button
-            onClick={stopRoulette}
-            disableInAction={true}
-            disabled={finish}
-          >
+          <Button disableInAction={true} disabled={finish}>
             ストップ
           </Button>
         ) : (
-          <Button onClick={startRoulette} disabled={finish}>
+          <Button type="button" onClick={startRoulette} disabled={finish}>
             {finish ? '抽選終了' : 'スタート'}
           </Button>
         )}
