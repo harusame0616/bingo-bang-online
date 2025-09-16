4e32afc36c0c27544af85b4ef84acdedd0734e91
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2025-09-16 17:28:24 +0900

    ビンゴカードリストのクラス名を修正し、グリッドの列数を明示的に指定

--- src/app/_components/bingo-card-list.tsx
+++ src/app/_components/bingo-card-list.tsx
@@ -4,7 +4,7 @@ export function BingoCardList({ children }: PropsWithChildren) {
 	return (
 		<ul
 			aria-label="ビンゴカード"
-			className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
+			className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
 		>
 			{children}
 		</ul>
